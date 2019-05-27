2ca8bda3ef2a09df40c7161ba63385e624a7afeb
Domas <domas.bitvinskas@me.com>
2019-05-28 01:06:29 +0300

    Better link

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,5 +2,5 @@ import React from 'react'
 import Container from '@amendable/core'
 
 export default (props) => (
-  <Container fontSize={1} {...props} />
+  <Container component='a' fontSize={1} {...props} />
 )
